9ccf119fa03f28b24f7c5f354a9fb02f6147da99
Victor <victor.valdivia.dev@gmail.com>
2024-07-26 18:11:48 -0400

    Initial Commit

---
+++ CursoIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CursoIOS
+//
+//  Created by Victor on 26-07-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CursoIOS/CursoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoIOSApp.swift
+//  CursoIOS
+//
+//  Created by Victor on 26-07-24.
+//
+
+import SwiftUI
+
+@main
+struct CursoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
